1db6a0f1548e09ebdbf771158a140d51fee85726
Krisk-3 <73527206+Krisk-3@users.noreply.github.com>
2021-06-11 18:45:31 +0600

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,16 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+    text("Hint: Performs a particular task !!", 703,176)
+    text("C U T N I F O N", 703,154)
+    text("Hint: Stores all the information!!", 100,260)
+    text("A T B A S D E A", 100,240)
+}
